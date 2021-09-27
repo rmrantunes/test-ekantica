f59d4a67f02f7dba15685a450067a8a93fe014d6
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-27 18:11:50 -0400

    refactor: put Header to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/GlobalStyle";
 import { theme } from "styles/theme";
+import { Header } from "components/Header";
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </ThemeProvider>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import { Header } from "components/Header";
 
 export default function Home() {
-  return <Header />;
+  return <main>gestão de clientes</main>;
 }
