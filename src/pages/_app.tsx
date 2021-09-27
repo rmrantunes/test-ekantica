219fc7c955e8d0fb552c92aa675a8c455952ad17
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-27 18:05:49 -0400

    feat: add ThemeProvider to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,13 @@
+import { ThemeProvider } from "styled-components";
 import GlobalStyle from "styles/GlobalStyle";
+import { theme } from "styles/theme";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Component {...pageProps} />
-    </>
+    </ThemeProvider>
   );
 }
 
