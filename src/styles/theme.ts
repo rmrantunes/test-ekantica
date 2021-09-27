7fd8a5c0b928a36c22438510b2227f88514cabec
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-27 19:22:59 -0400

    feat: add rounded & transition tokens

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -5,6 +5,12 @@ export const theme = {
     lightGray: "#F6F7FB",
     gray: "#6D828B",
   },
+  rounded: {
+    full: "9999px",
+  },
+  transition: {
+    default: "0.2s",
+  },
 } as const;
 
 export type Theme = typeof theme;
