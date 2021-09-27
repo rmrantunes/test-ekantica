b8d7dbe2a92b92926f4127bc434e087ad72ad97f
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-27 18:31:12 -0400

    refactor: fix GlobalStyle

--- src/styles/GlobalStyle.ts
+++ src/styles/GlobalStyle.ts
@@ -1,21 +1,25 @@
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  html,
-  body {
+  * {
     padding: 0;
     margin: 0;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+    box-sizing: border-box;
+  }
+
+  html,
+  body {
+    font-family: Inter, -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
       Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  body {
+    background: ${({ theme }) => theme.colors.lightGray};
   }
 
   a {
     color: inherit;
     text-decoration: none;
-  }
-
-  * {
-    box-sizing: border-box;
   }
 `;
 
