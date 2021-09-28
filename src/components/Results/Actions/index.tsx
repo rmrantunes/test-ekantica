16dd5f01fee620bf6eb8014462b9498ce0a77f0e
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-28 11:44:28 -0400

    feat: implement Dropdown to ResultActions

--- src/components/Results/Actions/index.tsx
+++ src/components/Results/Actions/index.tsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import { Button } from "components/Button";
 import { RadioInput } from "components/RadioInput";
+import { Dropdown } from "components/Dropdown";
+import { DropdownButton } from "components/Dropdown/Button";
 
 import * as S from "./styles";
 
 export function ResultsActions() {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
   return (
     <S.Wrapper>
       <RadioInput />
+      <Dropdown
+        label="Actions"
+        isOpen={isDropdownOpen}
+        setIsOpen={setIsDropdownOpen}
+      >
+        <DropdownButton>Action 1</DropdownButton>
+        <DropdownButton>Action 2</DropdownButton>
+        <DropdownButton>Action 3</DropdownButton>
+      </Dropdown>
       <Button>Aplicar</Button>
     </S.Wrapper>
   );
