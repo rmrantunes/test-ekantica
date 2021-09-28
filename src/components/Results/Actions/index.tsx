76362ee0bb30c1252abafa768a500f304c6d7f07
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-28 10:28:03 -0400

    refactor: add Wrapper to ResultActions

--- src/components/Results/Actions/index.tsx
+++ src/components/Results/Actions/index.tsx
@@ -1,9 +1,13 @@
 import { Button } from "components/Button";
+import { RadioInput } from "components/RadioInput";
+
+import * as S from "./styles";
 
 export function ResultsActions() {
   return (
-    <>
+    <S.Wrapper>
+      <RadioInput />
       <Button>Aplicar</Button>
-    </>
+    </S.Wrapper>
   );
 }
