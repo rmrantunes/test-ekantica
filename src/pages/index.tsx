182d1bfa5bf67d42085e38f720d05f7fdc53f28a
Rafael Antunes <rafaelmessias710@gmail.com>
2021-09-28 11:48:29 -0400

    feat: add Results to index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,14 @@
 import { OfficeBuildingIcon } from "@heroicons/react/outline";
+
 import { PageTitle } from "components/PageTitle";
 import { Main } from "components/pages/index/Main";
+import { Results } from "components/Results";
 
 export default function Home() {
   return (
     <Main>
       <PageTitle icon={<OfficeBuildingIcon />}>Gestão de clientes</PageTitle>
+      <Results />
     </Main>
   );
 }
